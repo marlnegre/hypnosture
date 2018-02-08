76e9f0ebc7d9fd08f767975b9055087ea4d192e6
Neil Marjun Martinez <martinezneilmarjun18@gmail.com>
2018-02-09 04:09:59 +0800

    Sign up files added

---
+++ app/src/main/java/com/google/hangouts/hypnosture/User.java
@@ -0,0 +1,55 @@
+package com.google.hangouts.hypnosture;
+
+/**
+ * Created by neil on 2/9/18.
+ */
+
+public class User {
+
+    String userId;
+    String username;
+    String email;
+    String password;
+
+    public User(){
+    }
+
+    public User(String userId, String username, String email, String password) {
+        this.userId = userId;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
